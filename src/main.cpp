651dbe4830697f49c9eafb8c4b5befee16e85a1d
Michael Haberler <git@mah.priv.at>
2026-01-26 18:36:52 +0100

    Add Wifi, C6 OTA update, MQTT broker

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
 #include <lvgl.h>
 #include <M5Unified.h>
+#include <ESPmDNS.h>
+#include <PicoMQTT.h>
+#include <PicoWebsocket.h>
+#include <WiFi.h>
+#include "ESP_HostedOTA.h"
+#include <SD_MMC.h>
+
 #include "display_driver.h"
 #include "ui.h"
+
+// SDIO pins for Tab5
+#define SDIO2_CLK GPIO_NUM_12
+#define SDIO2_CMD GPIO_NUM_13
+#define SDIO2_D0 GPIO_NUM_11
+#define SDIO2_D1 GPIO_NUM_10
+#define SDIO2_D2 GPIO_NUM_9
+#define SDIO2_D3 GPIO_NUM_8
+#define SDIO2_RST GPIO_NUM_15
+
+static const char *hostname = "picomqtt";
+static wl_status_t wifi_status = WL_STOPPED;
+
+extern PicoMQTT::Server mqtt;
 
 void setup()
 {
@@ -14,11 +35,67 @@ void setup()
   display_init();
   M5.Display.setBrightness(200);
   ui_init();
+
+  WiFi.setPins(SDIO2_CLK, SDIO2_CMD, SDIO2_D0, SDIO2_D1, SDIO2_D2, SDIO2_D3, SDIO2_RST);
+  // Release SD Pins from whatever
+  gpio_reset_pin((gpio_num_t)39);
+  gpio_reset_pin((gpio_num_t)40);
+  gpio_reset_pin((gpio_num_t)41);
+  gpio_reset_pin((gpio_num_t)42);
+  gpio_reset_pin((gpio_num_t)43);
+  gpio_reset_pin((gpio_num_t)44);
+
+  WiFi.STA.begin();
+
+  WiFi.STA.connect(WIFI_SSID, WIFI_PASS);
+  // Set SD_MMC Pins
+  SD_MMC.setPins(43, 44, 39, 40, 41, 42);
 }
 
 void loop()
 {
   M5.update();
+  wl_status_t ws = WiFi.STA.status();
+  if (ws ^ wifi_status)
+  {
+    wifi_status = ws; // track changes
+    switch (ws)
+    {
+    case WL_CONNECTED:
+      log_i("WiFi: Connected, IP: %s", WiFi.STA.localIP().toString().c_str());
+
+      if (updateEspHostedSlave())
+      {
+        // Restart the host ESP32 after successful update
+        // This is currently required to properly activate the new firmware
+        // on the ESP-Hosted co-processor
+        ESP.restart();
+      }
+      if (MDNS.begin(hostname))
+      {
+        MDNS.addService("mqtt", "tcp", MQTT_PORT);
+        MDNS.addService("mqtt-ws", "tcp", MQTTWS_PORT);
+        MDNS.addServiceTxt("mqtt-ws", "tcp", "path", "/mqtt");
+        mdns_service_instance_name_set("_mqtt", "_tcp", "PicoMQTT TCP broker");
+        mdns_service_instance_name_set("_mqtt-ws", "_tcp",
+                                       "PicoMQTT Websockets broker");
+      }
+      mqtt.begin();
+      break;
+    case WL_NO_SSID_AVAIL:
+      log_i("WiFi: SSID %s not found", WIFI_SSID);
+      break;
+    case WL_DISCONNECTED:
+      log_i("WiFi: disconnected");
+      break;
+    default:
+      log_i("WiFi status: %d", ws);
+      break;
+    }
+    delay(300);
+  }
+
   display_update();
-  delay(5);
+  mqtt.loop();
+  delay(1);
 }
---
+++ src/mqtt.cpp
@@ -0,0 +1,47 @@
+#include <PicoMQTT.h>
+#include <PicoWebsocket.h>
+
+WiFiServer tcp_server(MQTT_PORT);
+WiFiServer websocket_underlying_server(MQTTWS_PORT);
+PicoWebsocket::Server<::WiFiServer>
+    websocket_server(websocket_underlying_server);
+
+class CustomMQTTServer : public PicoMQTT::Server
+{
+    using PicoMQTT::Server::Server;
+
+public:
+    int32_t connected, subscribed, messages;
+
+protected:
+    void on_connected(const char *client_id) override
+    {
+        log_i("client %s connected", client_id);
+        connected++;
+    }
+    virtual void on_disconnected(const char *client_id) override
+    {
+        log_i("client %s disconnected", client_id);
+        connected--;
+    }
+    virtual void on_subscribe(const char *client_id, const char *topic) override
+    {
+        log_i("client %s subscribed %s", client_id, topic);
+        subscribed++;
+    }
+    virtual void on_unsubscribe(const char *client_id,
+                                const char *topic) override
+    {
+        log_i("client %s unsubscribed %s", client_id, topic);
+        subscribed--;
+    }
+    virtual void on_message(const char *topic,
+                            PicoMQTT::IncomingPacket &packet) override
+    {
+        log_i("message topic=%s", topic);
+        PicoMQTT::Server::Server::on_message(topic, packet);
+        messages++;
+    }
+};
+
+CustomMQTTServer mqtt(tcp_server, websocket_server);
